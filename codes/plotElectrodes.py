c2e7d79e94dd7ca44e1d615fe336a5ae3dcd040d
ramonitzzz <romina.a.d@hotmail.com>
2022-08-05 16:10:19 +0000

    add corrMaps sorted by electrodes

--- codes/plotElectrodes.py
+++ codes/plotElectrodes.py
@@ -9,8 +9,8 @@ import nibabel as nib
 from matplotlib import gridspec
 from scipy.cluster import hierarchy
 
-
-def sortCorrMaps(subj,corr_mat, freq):
+def sortCorrMaps(subj,corr_mat, freq, elecs):
+    
     gs = gridspec.GridSpec(5, 5)
     fig=plt.figure(figsize=(20,10))
 
@@ -18,13 +18,19 @@ def sortCorrMaps(subj,corr_mat, freq):
     Z = hierarchy.linkage(corr_mat, 'single')
     dn= hierarchy.dendrogram(Z,orientation='right')
     ax2.set_xticks([ ])
+    chanorders = [elecs[l] for l in dn["leaves"]]
 
     ax = fig.add_subplot(gs[:-1, 1:])
-    ax.imshow(corr_mat[:,dn["leaves"]])
-    ax.set_axis_off()
+    ordered_dat = corr_mat[:,dn["leaves"]]
+    ordered_dat = ordered_dat[dn["leaves"],:]
+    ax.imshow(ordered_dat)
+    ax.tick_params("both")
+    #ax.set_xticks([ ])
+    ax.set_yticks(dn["leaves"])
+    ax.set_yticklabels(chanorders, labelsize=0.5)
 
-    fig.align_ylabels()
     gs.update(wspace=0.1, hspace=0)
     plt.title(subj + freq +" task sorted") 
     plt.savefig("/home/jovyan/ieegmovie/corrMatrices/sorted/task/"+subj+"_task_"+freq+"-corrMat.png")
     plt.close(fig)
+
